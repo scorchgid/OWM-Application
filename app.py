48328048f97c85ad8e191dd31524489950999e53
Scorchgid <scorchgid@gmail.com>
2019-09-09 11:32:22 +0100

    Split authorise to make smaller functions better for testing

--- app.py
+++ app.py
@@ -27,24 +27,41 @@ def welcome():
 def weather():
     authorization = request.headers.get("AUTHORIZATION")
     print("authorization " + authorization)
-
     return render_template('weather.html')
 
 
 def authorise(key, token):
-    decoded_jwt = jwt.decode(token, key, 'HS256')
+    global location
+    decoded_jwt = decode(key, token)
+    url = url_builder(decoded_jwt)
+    data = open_weather_retrieval(url)
+    json = configure_data(data)
 
+
+def decode(key, token):
+    decoded_jwt = jwt.decode(token, key, 'HS256')
+    return decoded_jwt
+    # print("Unable to decode provided jwt. Key" + key + " Token: " + token)
+
+
+def url_builder(decoded_jwt):
     url_address = 'http://api.openweathermap.org/data/2.5/weather'
     query = '?q='
     location = decoded_jwt['location']
     app_id_prefix = '&APPID='
     application_id = decoded_jwt['appID']
     temp_cel = '&units=metric'
-
     url = url_address + query + location + app_id_prefix + application_id + temp_cel
+    return url
 
+
+def open_weather_retrieval(url):
     res = requests.get(url)
     data = res.json()
+    return data
+
+
+def configure_data(data):
     time_format = '%H:%M:%S'
     temp = str(data['main']['temp_max']) + ' C'
     time = str(datetime.utcfromtimestamp(data['sys']['sunset']).strftime(time_format))
