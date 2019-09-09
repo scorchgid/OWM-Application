aed4d0fc720faa3e512a21027550e7ec8ba29f52
Scorchgid <scorchgid@gmail.com>
2019-09-09 10:06:12 +0100

    Removed login, Separated functions so that test function and weather function run the authenticate function.

--- app.py
+++ app.py
@@ -1,25 +1,46 @@
+from datetime import datetime
+from flask import Flask, request, make_response, jsonify, Response, render_template
+
 import jwt
 import requests
-from datetime import datetime
-from flask import Flask, request, make_response, jsonify
 
 # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbiI6IkxlZWRzIiwiYXBwSUQiOiJjMDI0ZTg1Y2VmOGYwZmM5MjhjM2YwZWY4NWQ3YWQwMCJ9.y5w4khCBL-iBkSyjgLfmMcGImc7UdCmSAXaK5ZmyG6M
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'm.m@tesco.com'
+app.config['TOKEN'] = 'm.m@tesco.com'
 
 
-@app.route('/WEATHER')
+@app.route('/')
 def welcome():
     print('Please provide key: ')
-    encoded_jwt = input()
-    decoded_jwt = jwt.decode(encoded_jwt, app.config['SECRET_KEY'], 'HS256')
+    encoded_jwt_key = input()
+
+    print('Please provide token: ')
+    encoded_jwt_token = input()
+
+    authorise(encoded_jwt_key, encoded_jwt_token)
+    return "hello"
+
+
+@app.route('/weather', methods=['HEAD'])
+def weather():
+    authorization = request.headers.get("AUTHORIZATION")
+    print("authorization " + authorization)
+
+    return render_template('weather.html')
+
+
+def authorise(key, token):
+    decoded_jwt = jwt.decode(token, key, 'HS256')
+
     url_address = 'http://api.openweathermap.org/data/2.5/weather'
     query = '?q='
     location = decoded_jwt['location']
     app_id_prefix = '&APPID='
     application_id = decoded_jwt['appID']
     temp_cel = '&units=metric'
+
     url = url_address + query + location + app_id_prefix + application_id + temp_cel
 
     res = requests.get(url)
@@ -28,19 +49,6 @@ def welcome():
     temp = str(data['main']['temp_max']) + ' C'
     time = str(datetime.utcfromtimestamp(data['sys']['sunset']).strftime(time_format))
     return jsonify({'Max Temperature': temp, 'Sunset Time': time})
-
-
-@app.route('/WEATHER', methods=['HEAD'])
-def weather():
-    return 'Weather will go here'
-
-
-@app.route('/login')
-def login():
-    auth = request.authorization
-    if auth and auth.password == 'password':
-        return ''
-    return make_response('Could not verify', 401, {'WWW-Authenticate': "Basic realm=Login Required"})
 
 
 if __name__ == '__main__':
